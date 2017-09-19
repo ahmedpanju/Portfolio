14d10302f245fc466ded050c110162d211cb37e3
Ahmed Panju <ahmed.a.panju@gmail.com>
2017-09-19 13:15:47 -0400

    Update logic.js

--- Library/logic.js
+++ Library/logic.js
@@ -7,3 +7,16 @@ $(".css-copy").click(function(){
     $(".code-container-text-css").select();
     document.execCommand('copy');
 });
+
+$(document).ready(function(){
+    $(".navbar-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".navbar-header").offset().top
+        }, 1000);
+    });
+    $(".buttons-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".buttons-header").offset().top
+        }, 1000);
+    });
+});
